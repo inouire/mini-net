77ffea81a96cd108624b1c1641d9dd4ed28bd94c
inouire <edouard@melix.net>
2012-08-07 22:23:37 +0200

    Date display format improvements

--- src/Inouire/MininetBundle/Entity/Post.php
+++ src/Inouire/MininetBundle/Entity/Post.php
@@ -146,7 +146,7 @@ class Post{
      * Get content, with <br /> tags instead of carriage return
      */
     public function getHtmlContent(){
-        return str_replace("\n","<br />",$this->content);
+	return "<p>".str_replace("\n","</p>\n<p>",$this->content)."</p>";
     }
     
     /**
