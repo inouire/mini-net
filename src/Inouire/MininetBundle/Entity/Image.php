6d8506d814ed226e25d5fb670e9ca5b41a8ea07f
inouire <edouard@melix.net>
2012-09-02 15:18:27 +0200

    Improving edit post interface for picture upload

--- src/Inouire/MininetBundle/Entity/Image.php
+++ src/Inouire/MininetBundle/Entity/Image.php
@@ -85,7 +85,7 @@ class Image
     }
 
     public function getWebPath(){
-        return null === $this->path ? null : $this->getUploadDir().'/'.$this->path;
+        return null === $this->path ? null : '/'.$this->getUploadDir().'/'.$this->path;
     }
 
     public function getUploadRootDir(){
