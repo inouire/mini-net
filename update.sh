3a070dc872f399fb52b79d1f7478aa409319292f
inouire <edouard@melix.net>
2012-11-09 21:47:33 +0100

    Improving update script

--- update.sh
+++ update.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 echo "-------------------------------------------------------"
 echo "Updating your mini-net installation"
-echo "(use 'full' option to also update project dependencies)"
+echo "(use 'install' option to also install project dependencies)"
+echo "(use 'update' option to also update project dependencies)"
 echo "-------------------------------------------------------"
 
 echo "* Retrieving last version of mini-net from github"
 git pull
 
-if [ "$1" == "full" ]; then
-    echo "* Updating dependencies"
+if [ "$1" == "install" ]; then
+    echo "* Installing dependencies"
     composer install
 fi
-
+if [ "$1" == "update" ]; then
+    echo "* Updating dependencies"
+    composer update
+fi
 
 echo "* Clearing cache for env and prod environment"
 php app/console cache:clear --env=dev
