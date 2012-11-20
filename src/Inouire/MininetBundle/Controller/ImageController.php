b53ca190e399c35964e6ff9e4588d60052184074
inouire <edouard@melix.net>
2012-11-20 23:27:07 +0100

    Cleaner way to set cache directive in Image response

--- src/Inouire/MininetBundle/Controller/ImageController.php
+++ src/Inouire/MininetBundle/Controller/ImageController.php
@@ -204,6 +204,7 @@ class ImageController extends Controller
         
         //check that this image exists
         if($image==null ){
+            //TODO put a better error image file
             $image_file=__DIR__.'/../../../../web/css/icons/exit.png';
             $status_code=404;
         } else {
@@ -213,14 +214,18 @@ class ImageController extends Controller
         
         //prepare response
         $response = new Response();
-        $response->headers->set('ContentType','application/octet-stream');
         $response->headers->set('Accept-Ranges', 'bytes');
         $response->headers->set('Connection', 'keep-alive');
-        $response->headers->set('Content-Type','image/jpeg');
-        $response->headers->set('Cache-Control','private, max-age=3600, must-revalidate');
-        $response->headers->set('Content-Length',filesize($image_file));
         $response->setStatusCode($status_code);
+        
+        //set some cache informations
         $response->setPrivate();
+        $response->setMaxAge(3600);
+        $response->headers->addCacheControlDirective('must-revalidate', true);
+        
+        //set file content
+        $response->headers->set('Content-Type','image/jpeg');
+        $response->headers->set('Content-Length',filesize($image_file));
         $response->setContent(file_get_contents($image_file));
         
         return $response;
