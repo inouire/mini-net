e3ece3776f1e2176a2e90bbc3935d2688d14f35e
inouire <edouard@melix.net>
2013-01-10 21:52:18 +0100

    Refactoring of main classes and routing

--- src/Inouire/MininetBundle/Controller/ArchivesController.php
+++ src/Inouire/MininetBundle/Controller/ArchivesController.php
@@ -5,41 +5,43 @@ namespace Inouire\MininetBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Inouire\MininetBundle\Entity\Post;
 
-class AlbumController extends Controller
+class ArchivesController extends Controller
 {
-    
-    /*
-     * Redirects to the album of the current year
+
+/*
+     * Redirect archives root URL to the current year-month archives
      */
     public function viewCurrentMonthAction(){
-        return $this->redirect($this->generateUrl('album',array(
+        
+        return $this->redirect($this->generateUrl('posts',array(
             'year' => date('Y'),
             'month' => date('m')
         )));
+        
     }
     
     /*
-     * Handles the album of a given year
+     * Handles the archives of a given year and month
      */
-    public function viewAction($year,$month){
-        
+    public function postsAction($year,$month){
+
         //check validity of year and month given
         if( $year > 8000 || $year < 1 || $month < 1 || $month > 12){
             return $this->render('InouireMininetBundle:Main:errorPage.html.twig',array(
                 'error_title'=> 'Date invalide',
-                'error_message' => 'Impossible de générer l\'album du mois '.$month.' / année '.$year,
-                'follow_link' => $this->generateUrl('albums'),
-                'follow_link_text' => 'Aller à l\'album du mois en cours',
+                'error_message' => 'Impossible de récupérer les posts du mois '.$month.' / année '.$year,
+                'follow_link' => $this->generateUrl('archives'),
+                'follow_link_text' => 'Aller aux archives du mois en cours',
             ));
         }
         
-        //get entity manager and Image repository
+        //get entity manager and post repository
         $em = $this->getDoctrine()->getEntityManager();
-        $image_repo = $em->getRepository('InouireMininetBundle:Image');
+        $post_repo = $em->getRepository('InouireMininetBundle:Post');
         
-        //get all the images of the posts of the requested year
-        $image_list = $image_repo->getImagesOfMonth($year,$month);
-        
+        //retrieve posts of the requested month
+        $monthly_posts = $post_repo->getMonthlyPosts($year,$month);
+       
         //build date of current month
         $requested_date = new \Datetime($year.'-'.$month.'-01');
         
@@ -49,29 +51,24 @@ class AlbumController extends Controller
             $months_of_year[] = new \Datetime('2000-'.$m.'-01');
         }
         
-        //check that some pictures are avalaible for this year
-        if(count($image_list)>0){
-            //render the automatic album
-            return $this->render('InouireMininetBundle:Main:album.html.twig',array(
-                'image_list' => $image_list,
+        //check that there are some posts for this month
+        if( count($monthly_posts) > 0 ){
+            return $this->render('InouireMininetBundle:Main:archives.html.twig',array(
+                'post_list'=> $monthly_posts,
                 'requested_date' => $requested_date,
                 'requested_year' => $year,
                 'requested_month' => $month,
                 'months_of_year' => $months_of_year
             ));
-        }else{
-            //render the 'no album' page
-            return $this->render('InouireMininetBundle:Empty:noAlbum.html.twig',array(
+        }else{ //if no post available, display the specific page
+            return $this->render('InouireMininetBundle:Empty:noArchives.html.twig',array(
                 'requested_date' => $requested_date,
                 'requested_year' => $year,
                 'requested_month' => $month,
                 'months_of_year' => $months_of_year
             ));
         }
-
-
-
+        
     }
-    
     
 }
--- src/Inouire/MininetBundle/Controller/DefaultController.php
+++ src/Inouire/MininetBundle/Controller/DefaultController.php
@@ -8,9 +8,9 @@ use Inouire\MininetBundle\Entity\Post;
 class DefaultController extends Controller
 {
     /*
-     * Handles redirection of the root / of the website
+     * Handles redirection of the old 'home' url to the root / of the website
      */
-    public function rootAction(){
+    public function oldHomeAction(){
         return $this->redirect($this->generateUrl('home'));
     }
     
@@ -41,76 +41,12 @@ class DefaultController extends Controller
 
         if(count($post_list)==0){
             return $this->render('InouireMininetBundle:Empty:emptyHome.html.twig');
-    }else{
-            return $this->render('InouireMininetBundle:Main:home.html.twig',array(
-                'post_list'=> $post_list,
-                'post_secondary_list' => $post_secondary_list
-            ));
-    }
-    }
-    
-    /*
-     * Redirect archives root URL to the current year-month archives
-     */
-    public function archivesAction(){
-        
-        return $this->redirect($this->generateUrl('posts',array(
-            'year' => date('Y'),
-            'month' => date('m')
-        )));
-        
-    }
-    
-    /*
-     * Handles the archives of a given year and month
-     */
-    public function postsAction($year,$month){
-
-        //check validity of year and month given
-        if( $year > 8000 || $year < 1 || $month < 1 || $month > 12){
-            return $this->render('InouireMininetBundle:Main:errorPage.html.twig',array(
-                'error_title'=> 'Date invalide',
-                'error_message' => 'Impossible de récupérer les posts du mois '.$month.' / année '.$year,
-                'follow_link' => $this->generateUrl('archives'),
-                'follow_link_text' => 'Aller aux archives du mois en cours',
-            ));
+        }else{
+                return $this->render('InouireMininetBundle:Main:home.html.twig',array(
+                    'post_list'=> $post_list,
+                    'post_secondary_list' => $post_secondary_list
+                ));
         }
-        
-        //get entity manager and post repository
-        $em = $this->getDoctrine()->getEntityManager();
-        $post_repo = $em->getRepository('InouireMininetBundle:Post');
-        
-        //retrieve posts of the requested month
-        $monthly_posts = $post_repo->getMonthlyPosts($year,$month);
-       
-        //build date of current month
-        $requested_date = new \Datetime($year.'-'.$month.'-01');
-        
-        //build months of year
-        $months_of_year = array();
-        for( $m = 1 ; $m <=12 ; $m++){
-            $months_of_year[] = new \Datetime('2000-'.$m.'-01');
-        }
-        
-        //check that there are some posts for this month
-        if( count($monthly_posts) > 0 ){
-            return $this->render('InouireMininetBundle:Main:archives.html.twig',array(
-                'post_list'=> $monthly_posts,
-                'requested_date' => $requested_date,
-                'requested_year' => $year,
-                'requested_month' => $month,
-                'months_of_year' => $months_of_year
-            ));
-        }else{ //if no post available, display the specific page
-            return $this->render('InouireMininetBundle:Empty:noArchives.html.twig',array(
-                'requested_date' => $requested_date,
-                'requested_year' => $year,
-                'requested_month' => $month,
-                'months_of_year' => $months_of_year
-            ));
-        }
-        
     }
-    
     
 }
