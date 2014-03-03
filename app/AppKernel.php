4aa7b0c6afbbb4dd45d99218337fe6c9fcad3fb6
inouire <edouard@melix.net>
2014-03-03 22:35:59 +0100

    Remove imagein-bundle dependency

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new BCC\ExtraToolsBundle\BCCExtraToolsBundle(),
-            new Liip\ImagineBundle\LiipImagineBundle(),
             new Inouire\MininetBundle\InouireMininetBundle(),
             new Inouire\UserBundle\InouireUserBundle(),
         );
