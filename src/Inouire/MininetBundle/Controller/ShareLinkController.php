c3540bc6731ca5d62c6461c2c024d85bad05e611
inouire <edouard@melix.net>
2014-03-25 22:03:32 +0100

    Add route + controler to create a sharelink

---
+++ src/Inouire/MininetBundle/Controller/ShareLinkController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Inouire\MininetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Inouire\MininetBundle\Entity\Post;
+use Inouire\MininetBundle\Entity\ShareLink;
+
+class ShareLinkController extends Controller
+{
+    
+    /**
+     * Create new sharelink on some post attachments
+     */
+    public function newAction(Post $post){
+        
+        //get current user
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        
+        // check that there is not already a share link on this post created the same day
+        $em = $this->getDoctrine()->getManager();
+        $sharelink_repo = $em->getRepository('InouireMininetBundle:ShareLink');
+        $existing_sharelink = $sharelink_repo->findOneBy(array(
+            'post' => $post,
+            'creation_date' => new \DateTime(),
+            'author'=> $user)
+        );
+        
+        if($existing_sharelink == null){
+            // create new link
+            $fresh_link = new ShareLink();
+            $fresh_link->setAuthor($user);
+            $post->addShareLink($fresh_link);
+            
+            // persist new link
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($fresh_link);
+            $em->flush();
+            
+            $sharelink = $fresh_link;
+        }else{
+            $sharelink = $existing_sharelink;
+        }
+        
+        return $this->render('InouireMininetBundle:Main:errorPage.html.twig',array(
+            'error_title'=> 'New Share Link',
+            'error_message' => 'Le lien '.$sharelink->getToken().' a été créé.',
+            'follow_link' => $this->generateUrl('home'),
+            'follow_link_text' => 'Retour à l\'accueil',
+        )); 
+    }
+        
+    
+}
