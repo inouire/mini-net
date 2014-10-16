923a837a3415a7fa07f90fd1609535f39b0ff3c1
inouire <edouard@melix.net>
2014-10-17 00:28:18 +0200

    Display basic list of pending invitations

--- src/Inouire/MininetBundle/Controller/InvitationController.php
+++ src/Inouire/MininetBundle/Controller/InvitationController.php
@@ -15,9 +15,14 @@ class InvitationController extends Controller
         
         $em = $this->getDoctrine()->getManager();
         
-        // get all invitations
-        $invitation_repo = $em->getRepository('InouireUserBundle:Invitation');   
+        // get all invitations + all users
+        $invitation_repo = $em->getRepository('InouireUserBundle:Invitation');
+        $user_repo       = $em->getRepository('InouireUserBundle:User');   
         $invitations = $invitation_repo->findAll();
+        $users       = $user_repo->findAll();
+        
+        // cross the information to produce a full list (could be done with a join ?)
+        $list = $this->mergeUsersAndInvites($users,$invitations);
         
         // create invitation form
         $invitation = new Invitation();
@@ -25,10 +30,33 @@ class InvitationController extends Controller
         
         // render page
         return $this->render('InouireMininetBundle:Admin:invitations.html.twig',array(
-            'invitations' => $invitations,
+            'invitations' => $list,
             'form' => $form->createView()
         ));
 
+    }
+    
+    private function mergeUsersAndInvites($users, $invitations)
+    {
+        $list = array();
+        
+        $used_codes = array();
+        
+        // list all users and display the ones who are connected to a key
+        foreach($users as $user){
+            if($user->getInvitation() != null){
+                $used_codes[] = $user->getInvitation()->getCode();
+                $list[] = array('email'=>$user->getEmail(),'status'=>'registered');
+            }
+        }
+        
+        // list all invites and show them that are not in the previous list
+        foreach($invitations as $invitation){
+            if( ! in_array($invitation->getCode(), $used_codes)){
+                $list[] = array('email'=>$invitation->getEmail(),'status'=>'invited');
+            }
+        }
+        return $list;
     }
     
     /**
